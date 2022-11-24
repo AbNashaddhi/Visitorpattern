a90ed988a2d61c8d37a0129b94612376146abffd
AbNashaddhi <117175615+AbNashaddhi@users.noreply.github.com>
2022-11-24 15:23:37 +0530

    Add files via upload

---
+++ Visitor Pattern/Visitor Pattern/InvoiceMarktingSender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor_Pattern
+{
+    class InvoiceMarktingSender : INotificationSender
+    {
+        public void Accept(IVisitor user)
+        {
+            user.Visitor(this);
+
+        }
+
+        public void Send(string Message)
+        {
+            Console.WriteLine($"Notification sent:{ Message}");
+        }
+
+       
+    }
+}
