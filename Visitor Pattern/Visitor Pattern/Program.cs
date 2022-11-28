31998d221289fa757c5af0419c66a62e437d2bed
AbNashaddhi <117175615+AbNashaddhi@users.noreply.github.com>
2022-11-28 17:11:54 +0530

    Add files via upload

---
+++ Visitor Pattern/Visitor Pattern/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor_Pattern
+{
+    class Program
+    {
+        public static List<IVisitor> visitors =
+       new List<IVisitor>
+           {
+                new EmailVisitor(),
+                new TextVisitor(),
+                new Webvisitor()
+           };
+        public static void PrintOutputDetails(InvoiceMarktingSender marketSender, MarktingNotificationSender notification)
+        {
+           foreach (var visit in visitors)
+            {
+                if (marketSender!=null)
+                {
+                    marketSender.Accept(visit);
+                   
+                }
+                else if (notification != null)
+                {
+                    notification.Accept(visit);
+                }
+            }
+        }
+    static void Main(string[] args)
+        {
+                
+            var notificationSender1 = new InvoiceMarktingSender();
+            PrintOutputDetails(notificationSender1,null);
+            notificationSender1.Send("invoice");
+            var notificationSender2 = new MarktingNotificationSender();
+            PrintOutputDetails(null, notificationSender2);
+            notificationSender2.Send("Markting");
+            Console.ReadLine();
+        }
+    }
+}
